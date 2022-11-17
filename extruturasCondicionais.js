618bde2626fa010919959534d8ed1032a628ca9e
Allan Sampaio <lanbrendo93@gmail.com>
2022-11-16 23:16:36 -0300

    adicionando expressões lógicas

---
+++ extruturasCondicionais.js
@@ -0,0 +1,20 @@
+//1
+if(caminho === 5 && corro === 2)
+{
+   console.log('passo o dia bem');
+} 
+
+//2 
+if(levanto === 'cedo' || levanto < 8)
+{
+    console.log('saio tranquilo');
+} 
+
+//3
+if(!terminotrabalho || !tenhocompromisso)
+{
+    console.log('saio tarde');
+} 
+
+///////////////////////////////////////////////////
+
